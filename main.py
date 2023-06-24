98e538c8774b4e97acc8e08701cf5dca3b7f8fdc
Aarushi-Sachdeva <aarushisachd01@gmail.com>
2023-06-24 16:57:51 -0700

    working on adding flask framework

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# from flask import Flask, request, jsonify
-
-
-# app =Flask(__name__)
-# @app.route('/')
-
+from flask import Flask, request, jsonify
 import openai
 from dotenv import load_dotenv
 import os
+
+app = Flask(__name__)
+
 
 # load env variables from .env file
 load_dotenv()
@@ -14,19 +12,24 @@ load_dotenv()
 # access env vairables
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-question = input("Please ask a question: ")
+question = input("Write math text as if you were speaking it : ")
 prefix = "Turn the following into latex: "
 prompt = prefix + question
 
-MODEL = "gpt-3.5-turbo"
-response = openai.ChatCompletion.create(
-    model=MODEL,
-    messages=[
-        {"role": "system", "content": "you are"},
-        {"role": "user", "content": prompt}
-    ],
-    temperature=0,
-)
 
+@app.route('/openai/api', methods=['POST'])
+def openai_api():
+    data = request.get_json()
 
-print(response["choices"][0]["message"]["content"])
+    prompt = data.get('prompt')
+    MODEL = "gpt-3.5-turbo"
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "you are"},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0,
+    )
+
+    print(response["choices"][0]["message"]["content"])
