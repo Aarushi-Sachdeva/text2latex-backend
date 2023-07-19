406e3b7c4dc403336046cce32ef1a0182120685a
Aarushi-Sachdeva <aarushisachd01@gmail.com>
2023-07-19 15:40:45 -0700

    added input validation and api request error handling

--- app.py
+++ app.py
@@ -15,25 +15,38 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/', methods=['POST'])
 def openai_api():
+
+    # user prompt
     data = request.get_json()
+
+    # input validation
+    if not data or 'prompt' not in data or not data['prompt']:
+        return jsonify(error="Missing Prompt."), 400
+    elif type(data['prompt']) != "str":
+        return jsonify(error="Prompt must be a string"), 400
+    elif len(data['prompt']) > 1000:
+        return jsonify(error="Prompt must be less than 1000 characters"), 400
 
     PREFIX = "turn the following into latex code: "
 
     prompt = PREFIX + data.get('prompt')
     MODEL = "gpt-3.5-turbo"
-    response = openai.ChatCompletion.create(
-        model=MODEL,
-        messages=[
-            {"role": "system", "content": ""},
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0,
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": ""},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0,
+        )
 
-    # generated_text = response.json().get('generated_text')
+        # generated_text = response.json().get('generated_text')
 
-    # Return the generated text as JSON response
-    return response['choices'][0]['message']['content']
+        # Return the generated text as JSON response
+        return response['choices'][0]['message']['content']
+    except Exception as e:
+        return jsonify(error="An error occurred while communicating with the OpenAI API."), 500
 
 
 if __name__ == "__main__":
