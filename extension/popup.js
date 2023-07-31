967c65f223a1efe8d79bea92e5b6c7341892b552
Aarushi-Sachdeva <aarushisachd01@gmail.com>
2023-07-31 14:58:11 -0700

    all the essential functionality has been added to the assitant

--- extension/popup.js
+++ extension/popup.js
@@ -1,23 +1,7 @@
 
-
 let url = 'https://text2latex.onrender.com/';
-//   document.getElementById('submit-button').addEventListener('click', function() {
-//     var inputText = document.getElementById('search-input').value;
-//     console.log(inputText);
-//     // Do something with inputText
-//     var data = {
-//       "type": "question",
-//       "prompt": inputText
-//     };
-//     postPrompt(url,data)
-//     .then(data => {
-//         console.log('success:', data.response); 
-//         document.getElementById('output').innerHTML = data.response;
-//     })
-//     .catch((error) => console.error('error',error));
-//   });
 
-  function postPrompt(url = '', data = {},incomingChatLi){
+function postPrompt(url = '', data = {},incomingChatLi){
     const messageElement = incomingChatLi.querySelector("p");
     return fetch (url, {
         method: 'POST',
@@ -27,49 +11,78 @@ let url = 'https://text2latex.onrender.com/';
         },
         body: JSON.stringify(data)
     })
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log(data);}).catch (error => console.log(error));
     .then(response => {
         if(!response.ok){
+          messageElement.classList.add("error");
           messageElement.textContent = "Oops! Something went wrong. Please try again.";
             // throw new Error('Network response was not ok');
         }
         return response.json();
-    })
-    .then(data => {
-      messageElement.textContent = data['choices'][0]['message']['content'];
-      // response['choices'][0]['message']['content']
+    }).then(data => {
+      messageElement.textContent = data['response'];
       return data;
+    }).catch(error => {
+      messageElement.classList.add("error");
+      messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => {
+      chatbox.scrollTo(0,chatbox.scrollHeight);
     })
-}
+  };
 
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
+const closeChatBtn = document.querySelector(".chatbot header span");
 
 let userMessage;
+const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
   //create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", className);
-  let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+  let chatContent = className === "outgoing" ? `<p>$</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>$</p>`;
   chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
   return chatLi;
 }
 
 const handleChat = () => {
   userMessage = chatInput.value.trim();
   if(!userMessage) return;
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
 
   // append the user's message to the chatbox
   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-
+  chatbox.scrollTo(0,chatbox.scrollHeight);
   setTimeout(() => {
     //display "thinking...." message while waiting fr the response
     const incomingChatLi = createChatLi("Thinking....", "incoming")
     chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0,chatbox.scrollHeight);
     postPrompt(url, {"type": "question", "prompt": userMessage},incomingChatLi)
   },600);
 };
 
 sendChatBtn.addEventListener("click", handleChat);
+
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 400){
+    e.preventDefault();
+    handleChat();
+  }
+ });
+
+
+closeChatBtn.addEventListener("click", () => {window.close();})
+chatInput.addEventListener("input", () => {
+  //adjust the height of the input textarea based on the scrollheight
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
 
